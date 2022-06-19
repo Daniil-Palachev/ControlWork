3de7a5c9804ebd46285f373326057528007ace03
Daniil <danilaunited@gmail.com>
2022-06-19 15:08:08 +0300

    task

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Clear();
+string[] arrayStart = new string[5] { "Moscow", "Res", "Tip", "world", "Pit" };
+Console.WriteLine(String.Join(",", arrayStart));
+ThreeSymbols(arrayStart);
+
+void ThreeSymbols(string[] arrayStart)
+{
+    int count = 0;
+    string[] arrayEnd = new string[arrayStart.Length];
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+        if (arrayStart[i].Length <= 3)
+        {
+            arrayEnd[count] = arrayStart[i];
+            count++;
+        }
+    }
+    Console.WriteLine(String.Join(",", arrayEnd));
+}
+
+
+
+
+
+
